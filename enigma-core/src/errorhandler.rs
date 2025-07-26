1f2513b9cc224882f6519ca31b501334c26fbba4
Harshal292004 <malaniharshal95@gmail.com>
2025-07-26 08:43:25 +0530

    Updates | need to read the rust book again , its been a while

---
+++ enigma-core/src/errorhandler.rs
@@ -0,0 +1,8 @@
+pub struct ErrorHandler{
+
+}
+
+impl ErrorHandler{
+    
+}
+
--- enigma-core/src/main.rs
+++ enigma-core/src/main.rs
@@ -1,3 +1,36 @@
+pub mod lexer;
+pub mod  parser;
+use lexer::Lexer;
+use lexer::tokens::{Token,TokenType};
+use parser::Parser;
+use std::fs;
+
+fn get_token_stream(lex:Lexer){
+    
+   loop{
+
+    let mut token_type:TokenType;
+
+    let token=  lex.next();
+
+    match token.{
+        TokenType::Eof => None,
+        _ => Some(token),
+
+    }
+    if tok_type == TokenType::Eof{
+        break;
+    }
+   }
+    while tok_type!= TokenType::Eof {
+        
+    }
+}
 fn main() {
     println!("Hello, Mystic!");
+    let contents = fs::read_to_string(file_path)
+    .expect("Should have been able to read the file");
+
+    let lex= Lexer::new(&contents);
+    let parse =  Parser::new(token_stream);
 }
